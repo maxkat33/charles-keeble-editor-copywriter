4e89c6f065ee38ecf8bbf7dc449bf283b48e8664
maxkat33 <max.katilius@gmail.com>
2023-05-27 10:36:53 +1000

    issue with img tags not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ const App = () => {
   return (
     <main>
       <section>
+        <img
+          src="./assets/images/home-top.png"
+          alt="decorative-charcoal-banner"
+        />
         <h1 className="title">CHARLES E. KEEBLE</h1>
         <h3 className="subtitle">THE LAST COPYWRITER YOU'LL EVER HIRE!</h3>
         <Navbar />
-        <img src="./assets/images/home-bottom.png" />
+        <img
+          src="./assets/images/home-bottom.png"
+          alt="decorative-charcoal-banner"
+        />
       </section>
     </main>
   );
